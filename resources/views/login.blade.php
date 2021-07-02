21bd59a2a193eb7d82b25f93591b989fa5d5253b
Eduardo Flores <eduardo.flores@s2next.com>
2021-07-02 18:21:00 -0500

    Rutas y modificación al login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,6 @@
     <title>Chat</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <link rel="icon" href="{{asset('assets/img/favicon.svg')}}" type="image/x-icon"/>
-
-    <!-- Fonts and icons -->
     <script src="{{asset('assets/js/plugin/webfont/webfont.min.js')}}"></script>
     <script>
         WebFont.load({
@@ -17,113 +15,47 @@
             }
         });
     </script>
-
-    <!-- CSS Files -->
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/atlantis.min.css')}}">
-
-    <!-- CSS Just for demo purpose, don't include it in your project -->
     <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}">
-
-
-
-
-    <!-- css chat -->
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
-
-
-
-    <!-- CSS no -->
     <link rel="stylesheet" type="text/css" href="{{asset('vendors/styles/core.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('vendors/styles/icon-font.min.css')}}">
-
 </head>
-
-
-
 
 <body class="login-page">
 <div class="login-box">
-
-    <!-- /.login-logo -->
     <div class="login-box-body">
         <div class="text-center">
-            <img src="../assets/img/logo3.svg" class="logo-login" alt="">
+            <img src="{{asset('assets/img/logo3.svg')}}" class="logo-login" alt="">
         </div>
-
-
-
-        <form action="#" method="post" class="text-left login-form">
-
-
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
             <div class="form-group fontuser">
-                <label><b>E-mail</b></label>
-                <input type="text"
-                       placeholder="Ingrese su e-mail"
-                       name="uname" required>
+                <label for="email"><b>{{__('Email')}}</b></label>
+                <input type="email" class="form-control" placeholder="{{__('Ingrese su Email')}}" id="email" name="email" required>
                 <i class="fa fa-envelope fa-lg"></i>
             </div>
-
             <div class="form-group fontuser">
-                <label><b>Contraseña</b></label>
-                <input type="text"
-                       placeholder="Ingrese su contraseña"
-                       name="uname" required>
+                <label for="password">
+                    <b>{{__('Contraseña')}}</b>
+                </label>
+                <input type="password" placeholder="{{__('Ingrese su contraseña')}}" id="password" name="password" required>
                 <i class="fa fa-lock fa-lg"></i>
             </div>
-
-            <!-- <div class="form-group">
-                <label for="email2">E-mail</label>
-                <input type="email" class="form-control" id="" placeholder="Ingrese su E-mail">
-            </div>
-
-            <div class="form-group">
-                <label for="password">Contraseña</label>
-                <input type="password" class="form-control" id="" placeholder="Ingrese su contraseña">
-            </div> -->
-
             <div class="text-center">
-                <a href="login-establecer.html" class="btn btn-login btn-round mt-3">Ingresar</a>
+                <button type="submit" class="btn btn-login btn-round mt-3">
+                    {{ __('Login') }}
+                </button>
             </div>
         </form>
-
         <div class="mt-3">
-            <a href="login-reestablecer.html" class="forget">Olvidé mi contraseña</a>
+            <a href="{{route('login')}}" class="forget">{{__('Olvidé mi contraseña')}}</a>
         </div>
-
     </div>
 
-
-
-
-
-
-
-
-
-
-
-
-    <!--   Core JS Files   -->
-    <script src="../assets/js/core/jquery.3.2.1.min.js"></script>
-    <script src="../assets/js/core/popper.min.js"></script>
-    <script src="../assets/js/core/bootstrap.min.js"></script>
-
-    <!-- jQuery UI -->
-    <!-- <script src="../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
-    <script src="../assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script> -->
-
-    <!-- jQuery Scrollbar -->
-    <!-- <script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script> -->
-
-
-    <!-- Chart JS -->
-
-    <!-- jQuery Sparkline -->
-
-    <!-- Chart Circle -->
-
-    <!-- Datatables -->
-
+    <script src="{{asset('assets/js/core/jquery.3.2.1.min.js')}}"></script>
+    <script src="{{asset('assets/js/core/popper.min.js')}}"></script>
+    <script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+Auth::routes();
 
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::post('login', 'LoginController@index')->name('login');
 
 Route::get('/departments', function () {
     return view('departamentos');
 });
 
 Route::view('/user', 'usuarios');
+
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
